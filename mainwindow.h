b5530d14ed634136aacb0ffb3f8e3dc6e2e0d2e6
HibaFzz <61536445+HibaFzz@users.noreply.github.com>
2022-04-14 00:09:40 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include<commande.h>
 #include<contenir.h>
+#include <QTcpSocket>
+#include <QAbstractSocket>
 
 namespace Ui {
 class MainWindow;
@@ -28,14 +30,101 @@ private slots:
 
 
 
-    void on_Statistiques_clicked();
+
 
     void on_quitter_clicked();
+
+    void on_GestionCom_2_clicked();
+
+    void on_pushButton_clicked();
+
+
+    void on_Connecter_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_GestionCom_6_clicked();
+
+
+
+
+
+    void on_pushButton_Ajouter_clicked();
+
+    void on_supprimer_2_clicked();
+
+    void on_recu_pdf_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_modifier_2_clicked();
+
+    void on_IDCom_2_currentIndexChanged(int index);
+
+    void on_recherche_button_clicked();
+
+    void on_pdf_2_clicked();
+
+    void on_Actualiser_clicked();
+
+    void on_id_tri_clicked();
+
+    void on_dateR_tri_clicked();
+
+    void on_dateL_tri_clicked();
+
+    void on_pushButton_8_clicked();
+
+
+    void on_pushButton_17_clicked();
+
+
+
+
+
+    void on_ajouter_c_clicked();
+
+    void on_retour_clicked();
+
+    void on_passeC_clicked();
+
+    void on_recu_pdf_2_clicked();
+
+    void on_recherche_button_3_clicked();
+
+    void on_Statistiques_3_clicked();
+
+    void on_supprimer_5_clicked();
+
+    void on_pdf_clicked();
+
+    void on_home_clicked();
+
+    void on_home_2_clicked();
+
+    void on_quitter_4_clicked();
+
+    void on_radioButton_clicked();
+
+    void on_radioButton_2_clicked();
+
+    void on_Atualiser_clicked();
+
+    void on_quitter_9_clicked();
+
+    void on_quitter_13_clicked();
 
 private:
     Ui::MainWindow *ui;
     Commande C;
     Contenir Con;
+    QTcpSocket *socket; // Représente le serveur
 
 };
 
