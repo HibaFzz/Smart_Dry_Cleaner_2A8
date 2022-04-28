76082c2a4f941a6ee24f75b64a0d011dfb061eb1
CHargui01 <98750698+CHargui01@users.noreply.github.com>
2022-04-28 04:46:53 +0200

    Create moc_gestioncommandes.cpp

---
+++ integration_smart_dry_cleaner/debug/moc_gestioncommandes.cpp
@@ -0,0 +1,183 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'gestioncommandes.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.9)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../gestioncommandes.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'gestioncommandes.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.9. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_GestionCommandes_t {
+    QByteArrayData data[18];
+    char stringdata0[392];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_GestionCommandes_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_GestionCommandes_t qt_meta_stringdata_GestionCommandes = {
+    {
+QT_MOC_LITERAL(0, 0, 16), // "GestionCommandes"
+QT_MOC_LITERAL(1, 17, 23), // "on_GestionCom_6_clicked"
+QT_MOC_LITERAL(2, 41, 0), // ""
+QT_MOC_LITERAL(3, 42, 23), // "on_GestionCom_7_clicked"
+QT_MOC_LITERAL(4, 66, 21), // "on_GestionCom_clicked"
+QT_MOC_LITERAL(5, 88, 20), // "on_passerCom_clicked"
+QT_MOC_LITERAL(6, 109, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(7, 131, 31), // "on_pushButton_Ajouter_5_clicked"
+QT_MOC_LITERAL(8, 163, 21), // "on_recu_pdf_5_clicked"
+QT_MOC_LITERAL(9, 185, 19), // "on_passeC_5_clicked"
+QT_MOC_LITERAL(10, 205, 29), // "on_recherche_button_4_clicked"
+QT_MOC_LITERAL(11, 235, 23), // "on_Actualiser_4_clicked"
+QT_MOC_LITERAL(12, 259, 16), // "on_pdf_5_clicked"
+QT_MOC_LITERAL(13, 276, 19), // "on_id_tri_4_clicked"
+QT_MOC_LITERAL(14, 296, 22), // "on_dateR_tri_4_clicked"
+QT_MOC_LITERAL(15, 319, 22), // "on_dateL_tri_4_clicked"
+QT_MOC_LITERAL(16, 342, 24), // "on_pushButton_20_clicked"
+QT_MOC_LITERAL(17, 367, 24) // "on_pushButton_11_clicked"
+
+    },
+    "GestionCommandes\0on_GestionCom_6_clicked\0"
+    "\0on_GestionCom_7_clicked\0on_GestionCom_clicked\0"
+    "on_passerCom_clicked\0on_pushButton_clicked\0"
+    "on_pushButton_Ajouter_5_clicked\0"
+    "on_recu_pdf_5_clicked\0on_passeC_5_clicked\0"
+    "on_recherche_button_4_clicked\0"
+    "on_Actualiser_4_clicked\0on_pdf_5_clicked\0"
+    "on_id_tri_4_clicked\0on_dateR_tri_4_clicked\0"
+    "on_dateL_tri_4_clicked\0on_pushButton_20_clicked\0"
+    "on_pushButton_11_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_GestionCommandes[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   94,    2, 0x08 /* Private */,
+       3,    0,   95,    2, 0x08 /* Private */,
+       4,    0,   96,    2, 0x08 /* Private */,
+       5,    0,   97,    2, 0x08 /* Private */,
+       6,    0,   98,    2, 0x08 /* Private */,
+       7,    0,   99,    2, 0x08 /* Private */,
+       8,    0,  100,    2, 0x08 /* Private */,
+       9,    0,  101,    2, 0x08 /* Private */,
+      10,    0,  102,    2, 0x08 /* Private */,
+      11,    0,  103,    2, 0x08 /* Private */,
+      12,    0,  104,    2, 0x08 /* Private */,
+      13,    0,  105,    2, 0x08 /* Private */,
+      14,    0,  106,    2, 0x08 /* Private */,
+      15,    0,  107,    2, 0x08 /* Private */,
+      16,    0,  108,    2, 0x08 /* Private */,
+      17,    0,  109,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void GestionCommandes::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        GestionCommandes *_t = static_cast<GestionCommandes *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_GestionCom_6_clicked(); break;
+        case 1: _t->on_GestionCom_7_clicked(); break;
+        case 2: _t->on_GestionCom_clicked(); break;
+        case 3: _t->on_passerCom_clicked(); break;
+        case 4: _t->on_pushButton_clicked(); break;
+        case 5: _t->on_pushButton_Ajouter_5_clicked(); break;
+        case 6: _t->on_recu_pdf_5_clicked(); break;
+        case 7: _t->on_passeC_5_clicked(); break;
+        case 8: _t->on_recherche_button_4_clicked(); break;
+        case 9: _t->on_Actualiser_4_clicked(); break;
+        case 10: _t->on_pdf_5_clicked(); break;
+        case 11: _t->on_id_tri_4_clicked(); break;
+        case 12: _t->on_dateR_tri_4_clicked(); break;
+        case 13: _t->on_dateL_tri_4_clicked(); break;
+        case 14: _t->on_pushButton_20_clicked(); break;
+        case 15: _t->on_pushButton_11_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject GestionCommandes::staticMetaObject = {
+    { &QDialog::staticMetaObject, qt_meta_stringdata_GestionCommandes.data,
+      qt_meta_data_GestionCommandes,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *GestionCommandes::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *GestionCommandes::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_GestionCommandes.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int GestionCommandes::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 16;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
