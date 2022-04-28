e650e34c8937ad3b126ff6528de12a0da442a4e5
CHargui01 <98750698+CHargui01@users.noreply.github.com>
2022-04-28 04:44:19 +0200

    Create commande.h

---
+++ integration_smart_dry_cleaner/commande.h
@@ -0,0 +1,43 @@
+#ifndef COMMANDE_H
+#define COMMANDE_H
+
+#include<QString>
+#include<QSqlQueryModel>
+#include <QDate>
+class Commande
+{
+public:
+    Commande();
+    Commande(int,QDate,QDate,int,int);
+    int getID_COM();
+    QDate getDATE_RECEP();
+    QDate getDATE_LIVR();
+    void setID_COM(int);
+    void setDATE_RECEP(QDate);
+    void setDATE_LIVR(QDate);
+    bool ajouter ();
+    QSqlQueryModel* afficher();
+    bool supprimer(int);
+    bool modifier(int,QDate,QDate,int,int);
+    QSqlQuery rechercherCommande(int);
+    QSqlQueryModel* combo_comboclient();
+    QSqlQueryModel* combo_comboemploye();
+    QSqlQueryModel*combo_comboCom();
+    QSqlQueryModel * rechercher_multi(QDate,QDate,int,int,int);
+    QSqlQueryModel * trier_commande_id();
+    QSqlQueryModel * trier_commande_dtr(QString);
+    void genererpdf();
+    QSqlQueryModel * statistiques();
+    QSqlQueryModel * statistiques1();
+     bool rechercher_arduino(QByteArray );
+
+
+    /*QSqlQueryModel * trier_commande_dtl(QDate);*/
+
+
+private:
+    int ID_COM,FK_CLIENT,FK_EMPLOYE;
+    QDate DATE_RECEP;QDate DATE_LIVR;
+};
+
+#endif // COMMANDE_H
